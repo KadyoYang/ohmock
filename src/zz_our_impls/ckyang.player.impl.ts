ed6bc908ac61a4645f436bdb9045ac8b927eca49
양찬규 <yangchan00@ship-da.com>
2023-07-19 23:57:37 +0900

    [feat] ckyang impl 추가

--- src/zz_our_impls/ckyang.player.impl.ts
+++ src/zz_our_impls/ckyang.player.impl.ts
@@ -13,49 +13,137 @@ export default class CkYangPlayer implements OmPlayer {
       tactics: "둔다.",
     };
   }
+
   public async dropTheStone(
     fieldsStatus: FieldStatus,
     yourFlag: "O" | "X"
   ): Promise<Position2D> {
+    /** 무승부값[4,5,6,7] 흑승값[8] 7이 안전하고 적절한듯하다. 최강은 쓰지않겠다 */
+    const targetDepth = 7;
+    const positions = await this.getCriticalPositions(fieldsStatus, yourFlag);
+    if (positions.length === 1) {
+      return positions[0];
+    }
+
+    const positionsWithPoint: Array<Position2D & { point: number }> = [];
+    for (const position of positions) {
+      const copiedFieldStatus = this.deepCopy(fieldsStatus);
+      copiedFieldStatus.lastStonePosition = position;
+      copiedFieldStatus.fields[position.y][position.x] = yourFlag;
+      const point = await this.seeTheFuture(
+        copiedFieldStatus,
+        yourFlag,
+        this.flipTurn(yourFlag),
+        targetDepth,
+        0
+      );
+      positionsWithPoint.push({ x: position.x, y: position.y, point });
+    }
+
+    positionsWithPoint.sort((a, b) => {
+      return b.point - a.point;
+    });
+
+    return positionsWithPoint[0];
+  }
+
+  public async seeTheFuture(
+    fieldsStatus: FieldStatus,
+    yourFlag: "O" | "X",
+    turn: "O" | "X",
+    maxDepth: number,
+    depth: number = 0
+  ): Promise<number> {
+    if (depth > maxDepth) {
+      return 0;
+    }
+    if (fieldsStatus.fields.flat().every((fe) => fe !== "")) {
+      return 0;
+    }
+    let point = 0;
+    const candidates = await this.getCriticalPositions(
+      this.deepCopy(fieldsStatus),
+      turn
+    );
+
+    // 경우의 수를 둬본다
+    for (const candidate of candidates) {
+      // 둬본다 승리? 패배? 숫자 합산
+      // const copiedFieldStatus = this.deepCopy(fieldsStatus);
+      const copiedFieldStatus = fieldsStatus;
+      copiedFieldStatus.lastStonePosition = candidate;
+      copiedFieldStatus.fields[candidate.y][candidate.x] = turn;
+
+      if (
+        this.checkWinningCondition(
+          copiedFieldStatus.fields,
+          copiedFieldStatus.lastStonePosition,
+          5
+        )
+      ) {
+        point += yourFlag === turn ? 1 : -1;
+      } else {
+        // 결과 안나오네? 더 둬봐
+        point += await this.seeTheFuture(
+          copiedFieldStatus,
+          yourFlag,
+          this.flipTurn(turn),
+          maxDepth,
+          depth + 1
+        );
+      }
+      copiedFieldStatus.fields[candidate.y][candidate.x] = "";
+    }
+
+    return point;
+  }
+
+  public async getCriticalPositions(
+    fieldsStatus: FieldStatus,
+    yourFlag: "O" | "X"
+  ): Promise<Array<Position2D>> {
     const { fields, lastStonePosition } = fieldsStatus;
 
     // 첫수일때는 가운데에 둔다
     if (!lastStonePosition) {
-      return {
-        x: Math.floor(fields[0].length / 2),
-        y: Math.floor(fields.length / 2),
-      };
+      return [
+        {
+          x: Math.floor(fields[0].length / 2),
+          y: Math.floor(fields.length / 2),
+        },
+      ];
     }
 
     // 창
-    const attackPosition =
+    const attackPositions =
       this.runEMERGENCYDefenceAttackSystemTestingToolToWinTheGameAndMoneyBack(
         JSON.parse(JSON.stringify(fields)),
         yourFlag,
         5
       );
-    if (attackPosition) {
-      console.log("attackposition");
-      return attackPosition;
+    if (attackPositions && attackPositions.length > 0) {
+      return [attackPositions[0]];
     }
 
     // 방패
-    const defencePosition =
+    const defencePositions =
       this.runEMERGENCYDefenceAttackSystemTestingToolToWinTheGameAndMoneyBack(
         JSON.parse(JSON.stringify(fields)),
         yourFlag === "O" ? "X" : "O",
         5
       );
-    if (defencePosition) return defencePosition;
+    if (defencePositions && defencePositions.length > 0)
+      return defencePositions;
 
     // 선제 방패
-    const earlyDefencePosition =
+    const earlyDefencePositions =
       this.runEMERGENCYDefenceAttackSystemTestingToolToWinTheGameAndMoneyBack(
         JSON.parse(JSON.stringify(fields)),
         yourFlag === "O" ? "X" : "O",
         4
       );
-    if (earlyDefencePosition) return earlyDefencePosition;
+    if (earlyDefencePositions && earlyDefencePositions.length > 0)
+      return earlyDefencePositions;
 
     const myPointMap = this.makePointMap(fields.length, fields[0].length);
     const enemyPointMap = this.makePointMap(fields.length, fields[0].length);
@@ -102,7 +190,6 @@ export default class CkYangPlayer implements OmPlayer {
       return b.point - a.point;
     });
 
-    // console.dir(list, { depth: null });
     const printFields = (fields: number[][]): void => {
       const yMax = fields.length;
       const xMax = fields[0].length;
@@ -115,7 +202,14 @@ export default class CkYangPlayer implements OmPlayer {
     printFields(myPointMap);
     printFields(enemyPointMap);
 
-    return { x: list[0].x, y: list[0].y };
+    return list
+      .filter((l) => l.point === list[0].point && l.isMine === list[0].isMine)
+      .map((l) => {
+        return {
+          x: l.x,
+          y: l.y,
+        };
+      });
   }
 
   public makePointMap(yMax: number, xMax: number): number[][] {
@@ -185,7 +279,6 @@ export default class CkYangPlayer implements OmPlayer {
 
     for (let yi = 0; yi < yMax; yi++) {
       for (let xi = 0; xi < xMax; xi++) {
-        // console.log(yi, xi);
         for (const direction of [
           { x: -1, y: 1 },
           { x: 0, y: 1 },
@@ -283,9 +376,11 @@ export default class CkYangPlayer implements OmPlayer {
     fields: string[][],
     targetFlag: "O" | "X",
     targetCount: number
-  ): Position2D | null => {
+  ): Array<Position2D> => {
     const yMax = fields.length;
     const xMax = fields[0].length;
+
+    const candidates: Array<Position2D> = [];
 
     for (let yi = 0; yi < yMax; yi++) {
       for (let xi = 0; xi < xMax; xi++) {
@@ -293,13 +388,14 @@ export default class CkYangPlayer implements OmPlayer {
           fields[yi][xi] = targetFlag;
           const position = { y: yi, x: xi };
           if (this.checkWinningCondition(fields, position, targetCount)) {
-            return position;
+            candidates.push(position);
           }
+
           fields[yi][xi] = "";
         }
       }
     }
-    return null;
+    return candidates;
   };
 
   private checkWinningCondition = (
@@ -348,4 +444,9 @@ export default class CkYangPlayer implements OmPlayer {
     }
     return false;
   };
+
+  private deepCopy = <T extends object>(src: T): T =>
+    JSON.parse(JSON.stringify(src));
+
+  private flipTurn = (src: "O" | "X"): "O" | "X" => (src === "O" ? "X" : "O");
 }
