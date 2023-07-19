d36ed587d7f51540c6a77af1f9900c912e91f9a9
양찬규 <yangchan00@ship-da.com>
2023-07-19 20:57:59 +0900

    [refac] 필요없는 파일 제거 및 파일명 수정

--- src/zz_our_impls/index.ts
+++ src/zz_our_impls/index.ts
@@ -1,2 +1,4 @@
-export { default as RandomRandomRandom } from "./random.player.impl";
-export { default as CkyangPlayerImpl } from "./ckyang.player.impl";
+export { default as Lv1Player } from "./lv1.player.impl";
+export { default as Lv2Player } from "./lv2.player.impl";
+export { default as Lv3Player } from "./lv3.player.impl";
+export { default as Lv4Player } from "./lv4.player.impl";
